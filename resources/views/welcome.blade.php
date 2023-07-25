439a71ac72016cf1f5b286b96799e38a8d59232e
Muhammad Husni Mubarrok <mubarrokhusni12@gmail.com>
2023-07-25 09:38:33 +0700

    Update husni

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,7 @@
 <body>
     <div class="container text-center my-5">
         <h1 class="mb-4">Bimillah Paringi Lancar!</h1>
-        {{-- Contoh cara mereferensikan gambar di dalam file blade dengan menggunakan pendekatan Vite --}}
         <img class="img-thumbnail" src="{{ Vite::asset('resources/images/doalancar.PNG') }}" alt="image">
-
         <div class="col-md-2 offset-md-5 mt-4">
             <div class="d-grid gap-2">
                 <a href="{{route('home')}}" class="btn btn-dark" href="">Start</a>
